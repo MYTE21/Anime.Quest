86390e61b819e60e8b43cbb920c4b7c7c553051c
MYTE <yasmi.tohabar@gmail.com>
2023-06-15 03:32:53 +0600

    Open and close the driver once

--- scraper/anime_scraper.py
+++ scraper/anime_scraper.py
@@ -12,12 +12,13 @@ def anime_details():
 
 
 def get_all_anime():
-    start_page, end_page = 1, 5
+    driver = webdriver.Chrome()
+
+    start_page, end_page = 1, 51
     anime_list = []
+
     for page_id in range(start_page, end_page):
         url = f"https://www.anime-planet.com/anime/all?page={page_id}"
-
-        driver = webdriver.Chrome()
         driver.get(url)
 
         anime_names = driver.find_elements(By.CLASS_NAME, "cardName")
@@ -26,9 +27,9 @@ def get_all_anime():
             anime_name = row.text
             anime_list.append(anime_name)
 
-        driver.close()
-
     print(len(anime_list))
+
+    driver.close()
 
 
 if __name__ == "__main__":
