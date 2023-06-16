10fa5e387126c7a71a0526ad9b59df58b3249253
MYTE <yasmi.tohabar@gmail.com>
2023-06-17 02:31:02 +0600

    test created for anime_details

---
+++ tests/dummy_data/given_data.py
@@ -0,0 +1,9 @@
+urls = [
+        "https://www.anime-planet.com/anime/fullmetal-alchemist-brotherhood",
+        "https://www.anime-planet.com/anime/fruits-basket-the-final-season",
+        "https://www.anime-planet.com/anime/bleach-thousand-year-blood-war",
+        "https://www.anime-planet.com/anime/attack-on-titan-the-final-season-the-final-chapters",
+        "https://www.anime-planet.com/anime/yawaraka-atom",
+        "https://www.anime-planet.com/anime/eggroy",
+        "https://www.anime-planet.com/anime/hizukuri"
+]
