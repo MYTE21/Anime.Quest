438338d796d241d5baf821c568d9245db5ab5a05
MYTE <yasmi.tohabar@gmail.com>
2023-08-06 23:41:58 +0600

    nothing

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-
-username = st.secrets["username"]
-password = st.secrets["password"]
-cluster_name = st.secrets["cluster_name"]
-
-uri = f"mongodb+srv://{username}:{password}@{cluster_name}.yomhl8y.mongodb.net/?retryWrites=true&w=majority"
-
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-db = client["anime_quest"]
-collection = db["anime_country"]
-
-countries = collection.find()
-
-for country in countries[:10]:
-    st.write(country["Country"])
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
+#
+#
+# username = st.secrets["username"]
+# password = st.secrets["password"]
+# cluster_name = st.secrets["cluster_name"]
+#
+# uri = f"mongodb+srv://{username}:{password}@{cluster_name}.yomhl8y.mongodb.net/?retryWrites=true&w=majority"
+#
+# client = MongoClient(uri, server_api=ServerApi('1'))
+#
+# db = client["anime_quest"]
+# collection = db["anime_country"]
+#
+# countries = collection.find()
+#
+# for country in countries[:10]:
+#     st.write(country["Country"])
 
 st.write("DONE..!")
