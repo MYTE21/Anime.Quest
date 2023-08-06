cf8e171819b0563e92555ae9904da942c6beff4d
MYTE <yasmi.tohabar@gmail.com>
2023-08-07 00:04:20 +0600

    show again

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ username = st.secrets["username"]
 password = st.secrets["password"]
 cluster_name = st.secrets["cluster_name"]
 
-uri = f"mongodb+srv://{username}:{password}@{cluster_name}.yomhl8y.mongodb.net/?retryWrites=true&w=majority"
+st.write(username)
+st.write(password)
+st.write(cluster_name)
 
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-db = client["anime_quest"]
-collection = db["anime_country"]
-
-countries = collection.find()
-
-for country in countries[:10]:
-    st.write(country["Country"])
+# uri = f"mongodb+srv://{username}:{password}@{cluster_name}.yomhl8y.mongodb.net/?retryWrites=true&w=majority"
+#
+# client = MongoClient(uri, server_api=ServerApi('1'))
+#
+# db = client["anime_quest"]
+# collection = db["anime_country"]
+#
+# countries = collection.find()
+#
+# for country in countries[:10]:
+#     st.write(country["Country"])
 
 st.write("DONE..!")
