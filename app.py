69c478fba1bd32cdaf5f7ec302308ec33b09149b
MYTE <yasmi.tohabar@gmail.com>
2023-08-08 14:05:35 +0600

    page cofig

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+
+
+# Page Configuration
+st.set_page_config(
+    page_title="Anime Quest",
+    page_icon="assets/page_icon.png",
+    menu_items={
+        'About': "This is about section..!",
+        'Report a bug': "https://github.com/MYTE21/Anime.Quest/issues/new",
+    }
+)
 
 
 username = st.secrets["username"]
 password = st.secrets["password"]
 cluster_name = st.secrets["cluster_name"]
-
-st.write(username)
-st.write(password)
-st.write(cluster_name)
 
 uri = f"mongodb+srv://{username}:{password}@{cluster_name}.yomhl8y.mongodb.net/?retryWrites=true&w=majority"
 
